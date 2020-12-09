2a26ea74ef5ca5f3e2e4b0126ec9e6f891d0c639
Roger Hestad <roger@doublethink.no>
2020-12-09 23:01:04 +0100

    App: Adds support for table changing activeRows, etc

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
     h2 {{$t("general.heading")}}
     .content
       .small.chart
-        LineChart( :chart-data="dataCollection")
-        button(@click="fillData()") Randomize
-      DataTable.small.table(:rowHeadings="keyNames" :data="loadedData" :value="activeRows")
+        LineChart( :chart-data="dataCollection" :options="lineChartOptions")
+        button(@click="init()") Alle rader
+        button(@click="initRandomized()") Tilfeldige rader
+      DataTable.small.table(:rowHeadings="keyNames" :data="loadedData" :value="activeRows" @dataChanged="fillData")
 </template>
 
 <script>
@@ -22,152 +23,165 @@ export default {
     return {
       dataCollection: {},
       loadedData: [
-      {
-        "År": 0,
-        "Diesel": 8100,
-        "El": 12000,
-        "": ""
-      },
-      {
-        "År": 1,
-        "Diesel": 11355,
-        "El": 12825,
-        "": ""
-      },
-      {
-        "År": 2,
-        "Diesel": 14610,
-        "El": 13650,
-        "": ""
-      },
-      {
-        "År": 3,
-        "Diesel": 17865,
-        "El": 14475,
-        "": ""
-      },
-      {
-        "År": 4,
-        "Diesel": 21120,
-        "El": 15300,
-        "": ""
-      },
-      {
-        "År": 5,
-        "Diesel": 24375,
-        "El": 16125,
-        "": ""
-      },
-      {
-        "År": 6,
-        "Diesel": 27630,
-        "El": 16950,
-        "": ""
-      },
-      {
-        "År": 7,
-        "Diesel": 30885,
-        "El": 17775,
-        "": ""
-      },
-      {
-        "År": 8,
-        "Diesel": 34140,
-        "El": 18600,
-        "": ""
-      },
-      {
-        "År": 9,
-        "Diesel": 37395,
-        "El": 19425,
-        "": ""
-      },
-      {
-        "År": 10,
-        "Diesel": 40650,
-        "El": 20250,
-        "": ""
-      },
-      {
-        "År": 11,
-        "Diesel": 43905,
-        "El": 21075,
-        "": ""
-      },
-      {
-        "År": 12,
-        "Diesel": 47160,
-        "El": 21900,
-        "": ""
-      },
-      {
-        "År": 13,
-        "Diesel": 50415,
-        "El": 22725,
-        "": ""
-      },
-      {
-        "År": 14,
-        "Diesel": 53670,
-        "El": 23550,
-        "": ""
-      },
-      {
-        "År": 15,
-        "Diesel": 56925,
-        "El": 24375,
-        "": ""
-      },
-      {
-        "År": 16,
-        "Diesel": 60180,
-        "El": 25200,
-        "": ""
-      },
-      {
-        "År": 17,
-        "Diesel": 63435,
-        "El": 26025,
-        "": ""
-      },
-      {
-        "År": 18,
-        "Diesel": 66690,
-        "El": 26850,
-        "": ""
-      },
-      {
-        "År": 19,
-        "Diesel": 69945,
-        "El": 27675,
-        "": ""
-      },
-      {
-        "År": 20,
-        "Diesel": 73200,
-        "El": 28500,
-        "": ""
-      }
+        {
+          År: 0,
+          Diesel: 8100,
+          El: 12000,
+          "": ""
+        },
+        {
+          År: 1,
+          Diesel: 11355,
+          El: 12825,
+          "": ""
+        },
+        {
+          År: 2,
+          Diesel: 14610,
+          El: 13650,
+          "": ""
+        },
+        {
+          År: 3,
+          Diesel: 17865,
+          El: 14475,
+          "": ""
+        },
+        {
+          År: 4,
+          Diesel: 21120,
+          El: 15300,
+          "": ""
+        },
+        {
+          År: 5,
+          Diesel: 24375,
+          El: 16125,
+          "": ""
+        },
+        {
+          År: 6,
+          Diesel: 27630,
+          El: 16950,
+          "": ""
+        },
+        {
+          År: 7,
+          Diesel: 30885,
+          El: 17775,
+          "": ""
+        },
+        {
+          År: 8,
+          Diesel: 34140,
+          El: 18600,
+          "": ""
+        },
+        {
+          År: 9,
+          Diesel: 37395,
+          El: 19425,
+          "": ""
+        },
+        {
+          År: 10,
+          Diesel: 40650,
+          El: 20250,
+          "": ""
+        },
+        {
+          År: 11,
+          Diesel: 43905,
+          El: 21075,
+          "": ""
+        },
+        {
+          År: 12,
+          Diesel: 47160,
+          El: 21900,
+          "": ""
+        },
+        {
+          År: 13,
+          Diesel: 50415,
+          El: 22725,
+          "": ""
+        },
+        {
+          År: 14,
+          Diesel: 53670,
+          El: 23550,
+          "": ""
+        },
+        {
+          År: 15,
+          Diesel: 56925,
+          El: 24375,
+          "": ""
+        },
+        {
+          År: 16,
+          Diesel: 60180,
+          El: 25200,
+          "": ""
+        },
+        {
+          År: 17,
+          Diesel: 63435,
+          El: 26025,
+          "": ""
+        },
+        {
+          År: 18,
+          Diesel: 66690,
+          El: 26850,
+          "": ""
+        },
+        {
+          År: 19,
+          Diesel: 69945,
+          El: 27675,
+          "": ""
+        },
+        {
+          År: 20,
+          Diesel: 73200,
+          El: 28500,
+          "": ""
+        }
       ],
       xLabels: [],
       activeRows: [],
       datasets: [],
-      keyNames: []
-    }
+      keyNames: [],
+      lineChartOptions: {
+        responsive: true,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                min: 0,
+                max: 60000
+              }
+            }
+          ]
+        }
+      }
+    };
   },
-  mounted () {
+  mounted() {
     this.cleanData();
     this.extractKeyNames();
-    this.fillData();
+    this.init();
   },
   methods: {
     // * Removes empty object keys
-    cleanData(){
+    cleanData() {
       console.log("App.cleanData: loadedData = ", this.loadedData);
       for (const item in this.loadedData) {
         for (const [key] of Object.entries(this.loadedData[item])) {
           // console.log("key = ", key);//, ", value = ", value);
-          if(key == "") delete this.loadedData[item][key];
+          if (key == "") delete this.loadedData[item][key];
         }
       }
     },
@@ -180,8 +194,8 @@ export default {
      */
     initActiveRows(randomize = false) {
       this.activeRows = [];
-      for (var i = 0; i < this.loadedData.length; i++){
-        if(randomize) {
+      for (var i = 0; i < this.loadedData.length; i++) {
+        if (randomize) {
           this.activeRows[i] = Math.random() < 0.5; // randomizes active state
         } else {
           this.activeRows[i] = true;
@@ -193,8 +207,15 @@ export default {
       this.xLabels = this.getKeyValuesByIndex(0);
       console.log("xLabels = ", this.xLabels);
     },
-    fillData() {
+    initRandomized() {
       this.initActiveRows(true);
+      this.fillData();
+    },
+    init() {
+      this.initActiveRows();
+      this.fillData();
+    },
+    fillData() {
       this.initXLabels();
       this.generateDatasets();
       // console.log("xLabels length = ", this.xLabels.length, ", dataset[0] length = ", this.datasets[0].data.length);
@@ -202,21 +223,24 @@ export default {
       this.dataCollection = {
         labels: this.xLabels,
         datasets: this.datasets
-      }
+      };
     },
     generateDatasets() {
       this.datasets = [];
       let keyArray = Object.keys(this.loadedData[0]);
       keyArray = this.removeStringFromArray(keyArray, keyArray[0]); // remove first item/x axis
       console.log("keys = ", keyArray);
-      for(const key in keyArray) {
+      for (const key in keyArray) {
         console.log("key = ", keyArray[key]);
-        console.log("key values array = ", this.getKeyValuesByKey(keyArray[key]));
+        console.log(
+          "key values array = ",
+          this.getKeyValuesByKey(keyArray[key])
+        );
         this.datasets.push({
           label: keyArray[key],
           backgroundColor: this.getRandomColor(),
           data: this.getKeyValuesByKey(keyArray[key])
-        })
+        });
       }
 
       console.log("this.datasets = ", this.datasets);
@@ -229,22 +253,23 @@ export default {
     },
     getKeyValuesByKey(key) {
       let valuesArray = [];
-      for(var i = 0; i < this.loadedData.length; i++) {
-        if(this.activeRows[i])valuesArray.push(this.loadedData[i][key]);
+      for (var i = 0; i < this.loadedData.length; i++) {
+        if (this.activeRows[i]) valuesArray.push(this.loadedData[i][key]);
       }
       return valuesArray;
     },
     getKeyValuesByIndex(index) {
       let labelArray = [];
-      for  (var i = 0; i < this.loadedData.length; i++) {
+      for (var i = 0; i < this.loadedData.length; i++) {
         // console.log("item = ", Object.values(this.loadedData[item])[index]);
-        if(this.activeRows[i])labelArray.push(Object.values(this.loadedData[i])[index]);
+        if (this.activeRows[i])
+          labelArray.push(Object.values(this.loadedData[i])[index]);
       }
       return labelArray;
     },
     getRandomColor() {
-      var letters = '0123456789ABCDEF';
-      var color = '#';
+      var letters = "0123456789ABCDEF";
+      var color = "#";
       for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
       }
@@ -253,7 +278,7 @@ export default {
     getRandomInt() {
       return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
     }
-  },
+  }
 };
 </script>
 
@@ -278,5 +303,8 @@ export default {
   width: 45%;
   margin: 10px;
   margin-top: 50px;
+}
+button {
+  margin: 10px;
 }
 </style>
