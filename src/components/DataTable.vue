a0b254eef9bbaad933550d6ad4856608167c9b33
Roger Hestad <roger@doublethink.no>
2021-01-08 09:26:07 +0100

    DataTable: Refactors input checkbox to v-checkbox and toggleAll button

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -4,13 +4,14 @@
       table
         tr
           th(v-for="item in rowHeadings" :key="item") {{item}}
-          th Aktiv 
-            input(type="checkbox")
+          th
+            .checkbox-container
+              v-checkbox(v-model="allSelected" @click="onToggleAll" label="Aktiv")
         tr(v-for="(item, index) in data" :key="index")
           td(v-for="(itemValue, index) in Object.values(item)" ) {{ itemValue }}
           td
-            input(type="checkbox" v-model="value[index]" @change="onCheckboxChanged")
-            //- v-checkbox(v-model="value[i]" @change="onCheckboxChanged")
+            .checkbox-container
+              v-checkbox(v-model="value[index]" @change="onCheckboxChanged")
 </template>
 
 <script>
@@ -29,12 +30,38 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      allSelected: false
+    }
+  },
+  computed: {
+    allSelectedCheck() {
+      for (let i = 0; i < this.data.length; i++) {
+        if (!this.value[i]) return false;
+      }
+      return true;
+    }
+  },
+  watch: {
+    allSelectedCheck(newValue, oldValue) {
+      this.allSelected = newValue;
+    }
+  },
   methods: {
+    onToggleAll() {
+      for(let i = 0; i < this.value.length; i++) {
+        this.value[i] = this.allSelected;
+      }
+    },
     onCheckboxChanged() {
       let checked = [].concat(this.value);
       this.$emit("input", checked);
       this.$emit("dataChanged");
     },
+  },
+  mounted () {
+    this.allSelected = this.allSelectedCheck;
   },
 };
 </script>
@@ -48,5 +75,9 @@ table {
 td,
 th {
   border: 1px solid grey;
+}
+.checkbox-container {
+  display: flex;
+  justify-content: center;
 }
 </style>
