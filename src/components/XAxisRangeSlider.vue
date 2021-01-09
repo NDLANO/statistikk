5cbb267b5a434ec6ca1bb5b183e9bdb6a64bc4e5
Roger Hestad <roger@doublethink.no>
2021-01-09 15:51:04 +0100

    XaxisRangeSlider: Adds dummy component

---
+++ src/components/XAxisRangeSlider.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  div
+    v-row
+      v-col
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Array,
+      required: true
+    },
+    min: {
+      type: Number,
+      default: 0,
+    },
+    max: {
+      type: Number,
+      default: 1
+    },
+    ticks: {
+      type: String,
+      default: "always"
+    },
+    tickSize: {
+      type: Number,
+      default: 4
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
