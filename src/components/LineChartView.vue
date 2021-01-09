720db66da37eac79176a8017f409b3f395b2fc65
Roger Hestad <roger@doublethink.no>
2021-01-09 14:41:49 +0100

    LineChartView: Implements labels for X axis range slider

--- src/components/LineChartView.vue
+++ src/components/LineChartView.vue
@@ -34,26 +34,33 @@
         v-range-slider(
           @change="onXAxisSliderChange"
           v-model="xAxisValues"
-          :min="0"
-          :max="20")
+          :min="xAxisMin"
+          :max="xAxisMax"
+          ticks="always",
+          tick-size="4"
+          )
           template(v-slot:prepend)
-            v-text-field(
-              :value="xAxisValues[0]"
-              class="mt-0 pt-0"
-              hide-details
-              single-line
-              type="number"
-              style="width: 60px"
-              @change="$set(xAxisValues, 0, $event)")              
+            div
+              span(v-if="xAxisValues && dataCollection.labels") {{ dataCollection.labels[xAxisValues[0]]}}
+          //-   v-text-field(
+          //-     :value="xAxisValues[0]"
+          //-     class="mt-0 pt-0"
+          //-     hide-details
+          //-     single-line
+          //-     type="number"
+          //-     style="width: 60px"
+          //-     @change="$set(xAxisValues, 0, $event)")              
           template(v-slot:append)
-            v-text-field(
-              :value="xAxisValues[1]"
-              class="mt-0 pt-0"
-              hide-details
-              single-line
-              type="number"
-              style="width: 60px"
-              @change="$set(xAxisValues, 1, $event)")               
+            div
+              span(v-if="xAxisValues && dataCollection.labels") {{ dataCollection.labels[xAxisValues[1]]}}
+          //-   v-text-field(
+          //-     :value="xAxisValues[1]"
+          //-     class="mt-0 pt-0"
+          //-     hide-details
+          //-     single-line
+          //-     type="number"
+          //-     style="width: 60px"
+          //-     @change="$set(xAxisValues, 1, $event)")               
 </template>
 
 <script>
@@ -74,8 +81,11 @@ export default {
     return {
       yAxisValues: [0, 80000],
       xAxisValues: [0, 20],
+      xAxisMin: 0,
+      xAxisMax: 10,
       defaultYAxisValues: [0, 80000],
       defaultXAxisValues: [0, 20],
+
       lineChartOptions: {
         animation: {
           duration: 0,
@@ -111,8 +121,11 @@ export default {
     },
     onXAxisSliderChange(event) {
       const tmpOptions = JSON.parse(JSON.stringify(this.lineChartOptions));
-      tmpOptions.scales.xAxes[0].ticks.min = event[0];
-      tmpOptions.scales.xAxes[0].ticks.max = event[1];
+      tmpOptions.scales.xAxes[0].ticks.min = this.dataCollection.labels[event[0]];
+      tmpOptions.scales.xAxes[0].ticks.max = this.dataCollection.labels[event[1]];
+      // tmpOptions.scales.xAxes[0].ticks.min = event[0];
+      // tmpOptions.scales.xAxes[0].ticks.max = event[1];
+      console.log("tmpOptions = ", tmpOptions);
       this.lineChartOptions = tmpOptions;
     },
     resetChart() {
@@ -135,7 +148,35 @@ export default {
       this.resetYMax();
     },
   },
+  watch: {
+    dataCollection(newValue, oldValue) {
+
+      console.log("datacollection new value = ", newValue.labels, ", old value = ", oldValue);
+
+      var newXMinIndex = 0;
+      var newXMaxIndex = newValue.labels.length - 1;
+
+      console.log("dataCollection oldValue.length = ", oldValue);
+      if(oldValue.labels) {
+        var oldXMinValue = oldValue.labels[this.xAxisValues[0]];
+        console.log("dataCollection: oldXMinValue = ", oldXMinValue);
+        newXMinIndex = newValue.labels.indexOf(oldXMinValue);
+        if(newXMinIndex === -1) newXMinIndex = 0;
+
+        var oldXMaxValue = oldValue.labels[this.xAxisValues[1]];
+        newXMaxIndex = newValue.labels.indexOf(oldXMaxValue);
+        if(newXMaxIndex === -1) newXMaxIndex = newValue.labels.length - 1;
+      }
+      console.log("dataCollection: newXMinIndex = ", newXMinIndex);
+
+      this.xAxisValues = [newXMinIndex, newValue.labels.length - 1];
+      this.xAxisMin = 0;
+      this.xAxisMax = newValue.labels.length - 1;
+
+    }
+  },
   mounted() {
+    console.log("datacollection = ", this.dataCollection.labels[0]);
     this.init();
   },
 };
