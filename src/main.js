73b2797871159453ee9b57e759400faddfbc645b
Roger Hestad <roger@doublethink.no>
2021-01-15 09:39:30 +0100

    main: Imports Vue-html2canvas

--- src/main.js
+++ src/main.js
@@ -3,25 +3,27 @@ import VueI18n from "vue-i18n";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import VuePapaParse from "vue-papa-parse";
+import VueHtml2Canvas from "vue-html2canvas";
 import store from "./store";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueI18n);
 Vue.use(VuePapaParse);
+Vue.use(VueHtml2Canvas);
 
 const messages = {
-  no: window.statistikkDictionary
+  no: window.statistikkDictionary,
 };
 
 const i18n = new VueI18n({
   locale: "no",
-  messages
+  messages,
 });
 
 new Vue({
   i18n,
   vuetify,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
