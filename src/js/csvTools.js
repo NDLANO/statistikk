d23dfb45c2eb2ddddf5fdca1c2beffdc03afe73f
Roger Hestad <roger@doublethink.no>
2021-01-24 18:11:15 +0100

    Splits cleanCsvString into multiple functions for better readability

--- src/js/csvTools.js
+++ src/js/csvTools.js
@@ -1,16 +1,44 @@
 export function cleanCsvString(csvString) {
-  // ** Header number fix/hack start
-  // * resources https://dirask.com/posts/JavaScript-how-to-split-string-by-newline-k1wEQp
-  // * https://wsvincent.com/javascript-convert-array-to-string/
-
-  let fileLines = csvString.split(/\r\n|\n\r|\n|\r/);
+  // * Split string into lines
+  let fileLines = convertToLines(csvString);
   console.log("csvTools.cleanCsvString: First line = ", fileLines[0]);
-  let firstLineCommaArray = fileLines[0].split(",");
-  console.log(
-    "csvTools.cleanCsvString: first firstLineArray length = ",
-    firstLineCommaArray.length
-  );
-  let firstLineSemiArray = fileLines[0].split(";");
+
+  fileLines[0] = convertHeaderValuesToString(fileLines[0]);
+  console.log("csvTools.cleanCsvString First line = ", fileLines[0]);
+
+  fileLines = removeEmptyLastLine(fileLines);
+
+  csvString = fileLines.join("\r\n");
+
+  return csvString;
+}
+
+// ** Remove last line if empty
+export function removeEmptyLastLine(linesArray) {
+  let tmpArray = [...linesArray];
+  if (tmpArray[tmpArray.length - 1] == "") {
+    console.log("csvTools.removeEmptyLastLine: Last line is empty: Removing");
+    tmpArray.pop();
+    console.log(
+      "csvTools.removeEmptyLastLine: last line after removing = ",
+      tmpArray[tmpArray.length - 1]
+    );
+  }
+
+  return tmpArray;
+}
+
+// ** Header number fix/hack
+// * Adds a space after each element, ensuring the keys of the resulting JSON object
+// * gets sorted as strings, not numbers
+// *
+// * resources https://dirask.com/posts/JavaScript-how-to-split-string-by-newline-k1wEQp
+// * https://wsvincent.com/javascript-convert-array-to-string/
+export function convertHeaderValuesToString(singleLineArray) {
+  // * Split first line with both ; and ,
+  // * The longest one is most likely the delimiter
+  let firstLineCommaArray = singleLineArray.split(",");
+  let firstLineSemiArray = singleLineArray.split(";");
   let firstLineArray = undefined;
   let delimiter = undefined;
   if (firstLineSemiArray.length > firstLineCommaArray.length) {
@@ -24,24 +52,7 @@ export function cleanCsvString(csvString) {
   firstLineArray = firstLineArray.map((el) => {
     return el + " ";
   });
-  fileLines[0] = firstLineArray.join(delimiter);
-  console.log("csvTools.cleanCsvString First line = ", fileLines[0]);
-
-  // ** Remove last line if empty
-  if (fileLines[fileLines.length - 1] == "") {
-    console.log("csvTools.cleanCsvString: Last line is empty: Removing");
-    fileLines.pop();
-    console.log(
-      "csvTools.cleanCsvString: last line after removing = ",
-      fileLines[fileLines.length - 1]
-    );
-  }
-
-  csvString = fileLines.join("\r\n");
-  console.log("csvTools.cleanCsvString: firstLineArray = ", firstLineArray);
-  // ** Header number fix/hack end
-
-  return csvString;
+  return firstLineArray.join(delimiter);
 }
 
 export function convertToLines(csvString) {
