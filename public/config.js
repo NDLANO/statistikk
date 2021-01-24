b7abb1c0d005123b0e42fbfeec39cf75f182f19b
Roger Hestad <roger@doublethink.no>
2021-01-24 14:53:22 +0100

    Adds different versions of new dataset from NDLA

--- public/config.js
+++ public/config.js
@@ -8,5 +8,17 @@ window.mfNdlaConfig = {
       name: "Co2 utslipp",
       filename: "data/co2.csv",
     },
+    {
+      name: "BNP 1911 - 2011 Original",
+      filename: "data/Norge_1911.csv",
+    },
+    {
+      name: "BNP 1911 - 2011 med X-akse label",
+      filename: "data/Norge_1911_m_land.csv",
+    },
+    {
+      name: "BNP 1911 - 2011 roterte akser",
+      filename: "data/Norge_1911_rotert.csv",
+    },
   ],
 };
