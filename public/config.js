abc042cf062d316a3a9a3c17575c5b625b3828a2
Roger Hestad <roger@doublethink.no>
2021-02-09 16:51:42 +0100

    config: Comments out files that are not to be used and adds finished versions fo Arbeidsledighet and BNP

--- public/config.js
+++ public/config.js
@@ -1,20 +1,20 @@
 window.mfNdlaConfig = {
   datasets: [
-    {
-      name: "Testdata",
-      filename: "data/test.csv",
-    },
+    // {
+    //   name: "Testdata",
+    //   filename: "data/test.csv",
+    // },
     {
       name: "Arbeidsledighet",
-      filename: "data/Arbeidslediget-rotert4.csv",
+      filename: "data/arbeidsledighet.csv",
     },
+    // {
+    //   name: "Co2 utslipp",
+    //   filename: "data/co2.csv",
+    // },
     {
-      name: "Co2 utslipp",
-      filename: "data/co2.csv",
-    },
-    {
-      name: "BNP 1911 - 2011 roterte akser",
-      filename: "data/Norge_1911_rotert.csv",
+      name: "BNP 1911 - 2011",
+      filename: "data/bruttonasjonalprodukt.csv",
     },
   ],
 };
