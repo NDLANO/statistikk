500c5890567d9230301c73dd3c7a5cd1602ca451
Roger Hestad <roger@doublethink.no>
2021-02-16 15:11:57 +0100

    Adds language file for nynorsk

---
+++ public/nb-NN.js
@@ -0,0 +1,32 @@
+window.statistikkDictionaryNn = {
+  general: {
+    dataset: "Dataset",
+    chartType: "Diagramtype",
+    startButton: "Start",
+    nextButton: "Neste",
+    closeButton: "Lukk",
+    alt1Button: "A",
+    alt2Button: "B",
+    reset: "Tilbakestill diagram",
+    saveGraph: "Last ned bilde av diagram",
+    lineGraph: "Linje",
+    barGraph: "Stolpe",
+    importData: "Importer data",
+  },
+  import: {
+    title: "Last inn data",
+    fileTemplate: "Eksempelfil for data import (Excel)",
+    description:
+      "For å laste inn egne data må de lagres i en CSV-fil med en spesiell struktur. Klikk under for å finne ut hvordan du lager en fil med korrekt format.",
+    descriptionFile: "Instruksjoner (PDF)",
+    instruction: "Last inn datafil (.CSV)",
+    chooseFileButton: "Velg fil",
+    noFileChosen: "Ingen fil er valgt",
+    importButton: "Last inn",
+    closeWindowButton: "Avbryt",
+  },
+  table: {
+    toggleRows: "Rader av/på",
+    toggleColumns: "Kolonner av/på",
+  },
+};
